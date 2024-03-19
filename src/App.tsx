7b639588ebb1cc820e16252a27be528a7149f64e
mjh9401 <mjh9401@naver.com>
2024-03-19 15:35:37 +0900

    리덕스 Provider, useSelector, useDispatch 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
+import { useState } from 'react';
 import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from './reducers';
 
 type Props = {
   value : any;
@@ -7,15 +10,39 @@ type Props = {
 }
 
 function App({value,onIncrement,onDecrement}: Props) {
+  // useSelector : store 상태 가져오는 hooks
+  const counter = useSelector((state:RootState) => state.counter);
+  const todos : String[] = useSelector((state:RootState) => state.todos);
+  // useDispatch : store에 상태 보는 hooks
+  const dispatch = useDispatch();
+  const [todoValue, setTodoValue] = useState("");
+  const handleChange = (e : React.ChangeEvent<HTMLInputElement>) =>{
+    setTodoValue(e.target.value);
+  }
+  const addTodo = (e:React.FormEvent<HTMLFormElement>)=>{
+    e.preventDefault();
+    dispatch({type:"ADD_TODO", text:todoValue});
+    setTodoValue("");
+  }
+  
+
   return (
     <div className="App">
-      {/* Clicked : {value} tiems */}
+      Clicked : {counter} tiems
       <button onClick={onIncrement}>
         +
       </button>
       <button onClick={onDecrement}>
         -
       </button>
+      <ul>
+        {todos.map((todo,index) => <li key={index}>{todo}</li>)}
+      </ul>
+
+      <form onSubmit={addTodo}>
+        <input type="text" value={todoValue} onChange={handleChange}/>
+        <input type="submit" />
+      </form>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { createStore } from 'redux'
 import rootReducer from './reducers';
+import { Provider } from 'react-redux';
 
 
 const root = ReactDOM.createRoot(
@@ -13,18 +14,15 @@ const root = ReactDOM.createRoot(
 
 const store = createStore(rootReducer);
 
-store.dispatch({
-  type:"ADD_TODO",
-  text:"USE REDUX"
-})
-console.log('store.getState',store.getState());
 const render = () => root.render(
   <React.StrictMode>
-    <App 
-      value={store.getState()}
-      onIncrement={()=>store.dispatch({type:"INCREMENT"})}
-      onDecrement={()=>store.dispatch({type:"DECREMENT"})}
-    />
+    <Provider store={store}>
+      <App 
+        value={store.getState()}
+        onIncrement={()=>store.dispatch({type:"INCREMENT"})}
+        onDecrement={()=>store.dispatch({type:"DECREMENT"})}
+      />
+    </Provider>
   </React.StrictMode>
 );
 render();
